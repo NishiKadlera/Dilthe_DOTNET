356e9f0239cc9d8e64edf02b35146444e0bb4774
NishiKadlera <nishikadlera@gmail.com>
2022-04-19 18:23:30 +0530

    commit

---
+++ C#/CSharpTask2/CSharpTask2/ParamsArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTask2
+{
+    public class ParamsArray
+    {
+        public static void parray(params int[] a)
+        {
+            foreach (int i in a)
+            {
+                Console.WriteLine($"{i}");
+            }
+            Console.WriteLine();
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Params Array Concept");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine();
+            int size;
+            Console.WriteLine("Please enter the size of array");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[size];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter the values");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Elements in the array 'numbers' are:");
+            parray(numbers);
+
+        }
+       
+    }
+}
