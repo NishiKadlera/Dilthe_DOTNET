8c808888e30a5c28244e1631846fb5ee616c0a2c
NishiKadlera <nishikadlera@gmail.com>
2022-04-20 20:48:01 +0530

    commit

---
+++ C#/CSharpTask2/Sum1DArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTask2
+{
+    internal class Sum1DArray
+    {
+        public static void Main()
+        {
+            int size;
+            int sum=0, i;
+            Console.WriteLine("Give the size of array");
+            size =Convert.ToInt32(Console.ReadLine());
+
+            int[] arr=new int[size];
+            //to get input from user
+            Console.WriteLine("Enter the values");
+            for (i = 0; i < arr.Length;i++)         
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum+= arr[i];
+            }
+            Console.WriteLine($"Sum of natural number is: {sum}");
+
+
+        }
+    }
+}
