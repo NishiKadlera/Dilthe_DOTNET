5cd559fd9cb51f4030606d4a5c5d687f3f9282ae
NishiKadlera <nishikadlera@gmail.com>
2022-04-21 22:28:23 +0530

    commit

---
+++ C#/CSharpTask3/QueueDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTask3
+{
+    internal class QueueDemo
+    {
+        public static void Main()
+        {
+            Queue<string> queue = new Queue<string>();//creatig an instance
+
+            queue.Enqueue("One");
+            queue.Enqueue("Two");
+            queue.Enqueue("Three");
+            queue.Enqueue("Four");
+            queue.Enqueue("Five");
+
+            Console.WriteLine("QUEUE\n*****************************");
+            foreach(var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Top element in the Queue before Dequeue:{0}", queue.Peek());
+            Console.WriteLine("whether Six is present:{0}", queue.Contains("Six"));
+
+            queue.Dequeue();
+            Console.WriteLine("Queue after Dqueue");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Top element in the Queue after Dequeue:{0}",queue.Peek());
+            Console.WriteLine("whether Two is present:{0}", queue.Contains("Two"));
+        }
+        
+    }
+}
---
+++ C#/CSharpTask3/SumParamsArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTask3
+{
+    internal class SumParamsArray
+    {
+        /// <summary>
+        /// Sum of natural numbers using params array
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Params Array\n*********************************");
+
+            int size;
+            Console.WriteLine("Enter the size of array");
+            size =Convert.ToInt32(Console.ReadLine());
+
+            int[] natural=new int[size];
+
+            //To get the array elements from the user
+            Console.WriteLine("Enter the values");
+            for (int i = 0; i < natural.Length; i++)
+            {
+                natural[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.WriteLine();
+            Natural(natural);
+
+        }
+
+
+        public static void Natural(params int[] array)
+        {
+          
+            //To display the sum of entered array elements
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine($"The sum of the elements in array is:{sum} ");
+        }
+    }
+}
